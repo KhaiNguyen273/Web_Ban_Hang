84df20ec5e9fcf43d7a82c9d25df1e460ade9d3a
KhaiNguyen273 <luuphungkhainguyen2732005@gmail.com>
2025-07-21 22:04:41 +0700

    setup tool

---
+++ src/pages/Flashsale/index.js
@@ -0,0 +1,5 @@
+function Flashsale() {
+    return <h2>Flashsale page</h2>;
+}
+
+export default Flashsale;
---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+import { HeaderOnly } from '~/components/Layouts';
+import Home from '~/pages/Home';
+import Contact from '~/pages/Contact';
+import Cart from '~/pages/Cart';
+import Flashsale from '~/pages/Flashsale';
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/contact', component: Contact },
+    { path: '/cart', component: Cart },
+    { path: '/flashsale', component: Flashsale, layout: HeaderOnly },
+];
+
+const privateRoutes = [];
+
+export { privateRoutes, publicRoutes };
